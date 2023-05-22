73b8301a012837fbfe8a3a906fd33937d50c0670
Manish Fale <manishfale33@gmail.com>
2023-05-23 00:48:02 +0530

    first commit

---
+++ Newsblog/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('',include('home.urls')),
+    path('admin/', admin.site.urls),
+    path('froala_editor/',include('froala_editor.urls')),
+]
