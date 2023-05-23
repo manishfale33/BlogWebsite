08878fa6d84b525e83278f969fc6e149da8e27a6
Manish Fale <manishfale33@gmail.com>
2023-05-23 08:23:59 +0530

    static update

--- Newsblog/settings.py
+++ Newsblog/settings.py
@@ -118,7 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- home/models.py
+++ home/models.py
@@ -1,19 +1,24 @@
+from typing import Iterable, Optional
 from django.db import models
 from django.contrib.auth.models import User
 from froala_editor.fields import FroalaField
+from .helpers import *
 # Create your models here.
 
 
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
     content =FroalaField()
-    slug = models.SlugField(max_length=1000)
+    slug = models.SlugField(max_length=1000, null=True,blank=True)
     image = models.ImageField(upload_to='blog')
     created_at= models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-
+    
+    def save(self, *args, **kwargs):
+         self.slug = generate_slug()(self.title)
+         super(BlogModel, self).save(*args, **kwargs)
 
 
