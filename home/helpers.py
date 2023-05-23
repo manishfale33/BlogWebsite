772bd50982cea0e695accae0ec8a22d141f2bc29
Manish Fale <manishfale33@gmail.com>
2023-05-23 08:19:50 +0530

    static files

---
+++ home/helpers.py
@@ -0,0 +1,18 @@
+from django.utils.text import slugify
+
+import string
+import random
+
+
+def generate_random_string(N):
+    res= ''.join(random.choice(string.ascii_uppercase + 
+                               string.digits,k=N))
+    return res
+
+
+def generate_slug(text):
+    new_slug = slugify(text)
+    from home.models import BlogModel
+    if BlogModel.objects.filter(slug==new_slug).exists():
+      return generate_slug(text + generate_random_string(5))
+    return new_slug
