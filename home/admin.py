12d405e2e18bba259c5876ae9f430b04ac567b3a
Manish Fale <manishfale33@gmail.com>
2023-05-23 17:37:11 +0530

    update

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import BlogModel
-
+from .models import Category
 
 admin.site.register(BlogModel)
+admin.site.register(Category)
