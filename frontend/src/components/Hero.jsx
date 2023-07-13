e98ab1c6a1c6a5dfad90b5295899a9728eb2c661
Manish Fale <manishfale33@gmail.com>
2023-07-13 12:14:30 +0530

    update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,27 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Carousel from 'nuka-carousel';
 import Typed from 'react-typed';
 
+const Litebet = () => {
+  const carouselItems = [
+    './images/imageCar5.jpg',
+    './images/imageCar2.jpg',
+    './images/imageCar3.jpg',
+  ];
 
-const Hero = () => {
-  const backgroundImageUrl = 'url(" ")';
+  const [selectedImage, setSelectedImage] = useState(carouselItems[0]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [likes, setLikes] = useState(Array(carouselItems.length).fill(0));
+
+  const handleImageChange = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleLikeClick = (index) => {
+    setLikes((prevLikes) => {
+      const updatedLikes = [...prevLikes];
+      updatedLikes[index] += 1;
+      return updatedLikes;
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselItems.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [carouselItems.length]);
 
   return (
-    <div className='text-black' style={{ backgroundImage: backgroundImageUrl }}>
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'></p>
-          <Typed
-            className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['SHARE YOUR KNOWLEDGE', ' WITH', 'THE WORLD', 'AND', 'ENGAGE IN EXCITING DISCUSSIONS']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
-        </div>
-        <button className='bg-[#38bdf8] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'> write your own blog </button>
-      </div>
+    <div className="bg-cover bg-center bg-no-repeat w-full h-screen">
+      <Carousel
+        className="carousel"
+        slideIndex={currentSlide}
+        renderCenterLeftControls={({ previousSlide }) => (
+          <button
+            className="carousel-control relative"
+            onClick={previousSlide}
+          >
+            <span className="arrow-icon">
+              <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full bg-blue-500 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50"></span>
+              &#10094;
+            </span>
+          </button>
+        )}
+        renderCenterRightControls={({ nextSlide }) => (
+          <button className="carousel-control relative" onClick={nextSlide}>
+            <span className="arrow-icon">
+              <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full bg-blue-500 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50"></span>
+              &#10095;
+            </span>
+          </button>
+        )}
+        afterSlide={handleImageChange}
+        slideWidth="100%"
+        wrapAround={true}
+        swiping={true}
+        renderBottomCenterControls={null}
+      >
+        {carouselItems.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-center h-screen relative"
+          >
+            <img
+              src={item}
+              alt={`Carousel ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              {/* Typed Component */}
+              <div className="mb-8 text-center">
+                <p className="text-3xl font-bold pb-4 text-black"></p>
+                <Typed
+                  className="text-2xl font-bold"
+                  strings={[
+                    'SHARE YOUR KNOWLEDGE',
+                    'WITH',
+                    'THE WORLD',
+                    'AND',
+                    'ENGAGE IN EXCITING DISCUSSIONS',
+                  ]}
+                  typeSpeed={120}
+                  backSpeed={140}
+                  loop
+                />
+              </div>
+
+              {/* Write Blog button */}
+              <button className="bg-blue-500 hover:bg-blue-600 text-white text-lg font-semibold py-3 px-8 rounded-md transition duration-300 ease-in-out">
+                Write Your Own Blog
+              </button>
+            </div>
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
 
-export default Hero;
+export default Litebet;
