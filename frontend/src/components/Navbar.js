4d4ecb21cd86774d2940ad0ae7faa205596d7779
Manish Fale <manishfale33@gmail.com>
2023-08-15 23:03:12 +0530

    15/08/23

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -27,25 +27,25 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-       {/* Mobile Navigation Menu */}
-       <ul className={`${nav ? 'fixed left-0 top-0 w-[60%] h-11/12 bg-[#ffff] border-r border-r-gray-900 ease-in-out duration-500 z-50' : 'ease-in-out duration-500 fixed left-[-100%]'} md:hidden`}>
+      {/* Mobile Navigation Menu */}
+      <ul className={`${nav ? 'fixed left-0 top-0 w-[60%] h-11/12 bg-[#ffff] border-r border-r-gray-900 ease-in-out duration-500 z-50' : 'ease-in-out duration-500 fixed left-[-100%]'} md:hidden`}>
         <h1 className='w-full text-3xl font-bold text-[#38bdf8] m-4'>MUNity Blogs</h1>
-        <li className='p-4 border-b border-gray-600'>Home</li>
-        <li className='p-4 border-b border-gray-600'>GeoPolitics</li>
-        <li className='p-4 border-b border-gray-600'>Economics</li>
-        <li className='p-4 border-b border-gray-600'>StockMarket</li>
-        <li className='p-4 border-b border-gray-600'>SupplyChain</li>
-        <li className='p-4 border-b border-gray-600'>LogIn</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>Home</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>GeoPolitics</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>Economics</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>StockMarket</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>SupplyChain</li>
+        <li className='p-4 border-b border-gray-600 hover:bg-gray-100 transition duration-300'>LogIn</li>
       </ul>
 
       {/* Desktop Navigation */}
       <ul className='hidden md:flex space-x-4'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>GeoPolitics</li>
-        <li className='p-4'>Economics</li>
-        <li className='p-4'>StockMarket</li>
-        <li className='p-4'>SupplyChain</li>
-        <li className='p-4'>LogIn</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>Home</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>GeoPolitics</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>Economics</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>StockMarket</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>SupplyChain</li>
+        <li className='p-4 hover:bg-gray-100 transition duration-300'>LogIn</li>
       </ul>
 
       {/* Search Bar Icon */}
